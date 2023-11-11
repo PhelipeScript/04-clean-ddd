28b8758757cc1402bf1b90455693d8e488602eca
Phelipe Pereira <phelipescript@gmail.com>
2023-11-11 20:35:46 -0300

    Fundamentos de DDD - Mapeando propriedades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,15 @@
-import { randomUUID } from "node:crypto";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
-  authorId: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}

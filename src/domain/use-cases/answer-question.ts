a8a481de8cf1daef2f1e4d6d17466d5308303fcc
Phelipe Pereira <phelipescript@gmail.com>
2023-11-12 12:29:49 -0300

    Forum com DDD & Clean Architecture - Configuracao do ESLint

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,18 +1,15 @@
-import { Answer } from "../entities/answer";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: string;
-  questionId: string;
-  content: string;
+  instructorId: string
+  questionId: string
+  content: string
 }
 
 export class AnswerQuestionUseCase {
-
-  constructor(
-    private answersRepository: AnswersRepository
-  ) {}
+  constructor(private answersRepository: AnswersRepository) {}
 
   async execute({
     content,
@@ -25,8 +22,8 @@ export class AnswerQuestionUseCase {
       content,
     })
 
-    await this.answersRepository.create(answer);
+    await this.answersRepository.create(answer)
 
-    return answer;
+    return answer
   }
 }
